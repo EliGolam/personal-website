387116a4d959042687306459683c782f448cbcb5
EliGolam <elistasibusiness@gmail.com>
2022-12-09 13:45:17 +1100

    Added and tested TailWind in the project

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h1>Testing Vue / Vite and Hot Reload</h1>
+    <h1 class="text-3xl text-purple-600 font-bold">Testing Vue / Vite and Hot Reload by {{ name }}</h1>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        name: 'Elias Mahfuzul Golam',
+      }
+    }
   }
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       // Laravel 9
       laravel({
           input: [
-            'resources/css/app.css',
+            // 'resources/css/app.css',
             'resources/js/app.js',
           ],
           refresh: true,
