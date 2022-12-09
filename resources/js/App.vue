63db66a4d02ab4c07f12c1dc41161c76ef09890a
EliGolam <elistasibusiness@gmail.com>
2022-12-09 14:26:10 +1100

    Setup and tested PostCSS

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h1 class="text-3xl text-purple-600 font-bold">Testing Vue / Vite and Hot Reload by {{ name }}</h1>
+
+    <div class="test-container">
+      <p class="test-par">This is inside the container</p>
+    </div>
+
+    <p class="test-par">This is outside the container</p>
+    <p class="test-par my-ass">Hello</p>
   </div>
 </template>
 
@@ -14,6 +21,16 @@
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="postcss" scoped>
+
+.test-container {
+
+  background-color: yellow;
+  .test-par {
+    color: red;
+  }
+}
+
+
 
 </style>
